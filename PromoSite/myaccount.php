952328429c89dd7697993b51070c76467b4a36be
Jocelyn100077826 <jocelynngshienlin@gmail.com>
2019-05-05 20:36:53 +0800

    User account page and logo change
    User account page and logo change

---
+++ PromoSite/myaccount.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Promo</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1.0" />
+    <!-- Bootstrap -->
+
+    <link rel="icon" type="image/png" href="images/Logo.png"/>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css.map">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css.map">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css.map">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css.map">
+    <link rel="stylesheet" href="css/bootstrap.css.map">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="header">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="images/navnav.png" height="30" alt="PromoAlert Logo">
+        </a>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="signin.html">
+                <?php echo "hihihi"; ?>
+              </a>
+            </li>
+          </ul>
+            </div>
+        </nav>
+    </div>
+
+<div class="container">
+
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+
+</body>
+</html>

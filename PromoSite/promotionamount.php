3c3ff9a6c8c6a427cbb22d815dfe7b23391d447b
WilloneLim <4320719@students.swinburne.edu.my>
2019-05-13 20:24:54 +0800

    Updated graph v2

--- PromoSite/promotionamount.php
+++ PromoSite/promotionamount.php
@@ -1,7 +1,7 @@
 <?php 
 
-$connect = mysqli_connect("localhost", "root", "", "testing");
-$query = "SELECT * FROM account";
+$connect = mysqli_connect("localhost", "root", "", "promoalert");
+$query = "SELECT * FROM statistics";
 $result = mysqli_query($connect, $query);
 $chart_data = '';
 while($row = mysqli_fetch_array($result))
@@ -36,11 +36,11 @@ $chart_data = substr($chart_data, 0, -2);
     <link rel="stylesheet" type="text/css" href="css/main.css">
    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.1/dist/html2canvas.min.js"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css" rel="stylesheet"/>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.1/dist/html2canvas.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css" rel="stylesheet"/>
 </head>
 <body>
     <div class="header">
@@ -64,47 +64,60 @@ $chart_data = substr($chart_data, 0, -2);
               <img class="img-fluid" src="images/promo5kfc.png" alt="tealiveid">
               <p>20% off Chicken Nuggets and Tenders</p>
           </div>
+          <!--
           <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 title">
               <button class="btn1">Promotion Claims</button>
               <button class="btn2">Purchase Amount</button>
-          </div>
+          </div> 
+          -->
       </div>
         
      <div class="row">
-      <h1 align="center"> Profit for 2018</h1>
-      <br/><br/>
-         
+      <h1 align="center"> Promotion Statistics</h1>
+     </div>
+     <br/>
+        
+     <div id="prof" class="row grid-body no-border">
+        <div id="chartLegend" class="line-chart-legend"></div>
+        <div id="chart" style="width:800px;"></div>   
      </div>
         
-     <div class="row">
-        <div id="chart" style="width:800px;"></div>
-         
+    <div class="row"><button class="download" onclick="saveAsPDF();">Download</button> 
     </div>
-   <button class="download" onclick="saveAsPDF();">Download</button> 
     </div>
     
      <script>
-      Morris.Line({
+      var lineChart = Morris.Line({
         element : 'chart',
         data:[<?php echo $chart_data; ?>],
         xkey:'year',
         ykeys:['profit', 'purchase', 'sale'],
         labels:['Profit', 'Purchase', 'Sale'],
         hideHover:'auto',
+        smooth:true,
+        redraw:true,
+        resize:true,
+        preUnits: 'RM ',
         stacked:true
       });
+         
+      lineChart.options.labels.forEach(function(label, i) {
+        var legendItem = $('<span class="legend-item"></span>').text( label).prepend('<span class="legend-color">&nbsp;</span>');
+        legendItem.find('span')
+          .css('backgroundColor', lineChart.options.lineColors[i]);
+        $('#chartLegend').append(legendItem)
+   });
    
          function saveAsPDF() {
-    html2canvas(document.getElementById('chart')).then(canvas => {
+    html2canvas(document.getElementById('prof')).then(canvas => {
         var w = document.getElementById("chart").offsetWidth;
         var h = document.getElementById("chart").offsetHeight;
 
-        var img = canvas.toDataURL("image/jpeg", 1);
+        var img = canvas.toDataURL("image/png", 1);
 
-        var doc = new jsPDF('L', 'pt', [860, 400]);
-        doc.text(10, 10, 'Profit for 2018');
-        doc.addImage(img, 'JPEG', 20, 20, w, h);
-        doc.save('sample-file.pdf');
+        var doc = new jsPDF('L', 'pt', [660, 400]);
+        doc.addImage(img, 'PNG', 20, 20, w, h);
+        doc.save('Statistics.pdf');
     }).catch(function(e) {
         console.log(e.message);
     });

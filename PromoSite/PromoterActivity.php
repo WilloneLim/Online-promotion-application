d00e4e2c9c4ba10aea439c5717413c686bb3a22c
Stella97 <31676244+Stella97@users.noreply.github.com>
2019-05-15 08:33:11 +0800

    Database data link and JS script for clickable data

--- PromoSite/PromoterActivity.php
+++ PromoSite/PromoterActivity.php
@@ -1,3 +1,27 @@
+<?php
+
+if(isset($_POST['search']))
+{
+
+	 $valueToSearch = $_POST['valueToSearch'];
+	 $query = "SELECT * FROM `promotion` WHERE CONCAT(`promo_id`, `promo_title`) LIKE '%".$valueToSearch."%'";
+     $search_result = filterTable($query);
+}else {
+	 $query = "SELECT * FROM `promotion`";
+	 $search_result = filterTable($query);
+}
+
+function filterTable($query)
+{
+	$connect = mysqli_connect("localhost", "root", "", "promoalert");
+	$filter_Result = mysqli_query($connect, $query);
+	return $filter_Result;
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,6 +45,13 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css.map">
     <link rel="stylesheet" href="css/main.css">
+	
+    <style>
+	.data {
+		list-style-type: none;
+		
+	}
+	</style>
 </head>
 <body>
     <div class="header">
@@ -40,15 +71,21 @@
     
 <div class="container">
   <div class="container">
+	  <form action="PromoterActivity.php" method="POST">
   <div class="row">
   <div class="col-sm-4">
-  <img src="images/promo5kfc.png" alt="tealiveid" id="imginfo" style="width:200px;height:200px">
+	    <br><br>
+  <img src="images/promo5kfc.png" alt="tealiveid" id="imginfo" style="width:300px;height:200px">
   </div>
   <div class="col-sm-6">
- <h3> Kentucky Fried Chicken</h3>
+	   <hr class="w-100 mt-5" />
+ <h3 class="text-center mb-2"> Kentucky Fried Chicken</h3>
+	   <hr class="w-80 mt-5" />
+	  <ul class="data">
  <li>Last Online:</li>
  <li>No. Of Promotions:</li>
  <li>Average Claims:</li>
+	  </ul>
  
   </div>
   <div class="col-sm-2">
@@ -59,18 +96,18 @@
   </div>
   </div>
   </br>
+	  <form action="PromoterActivity.php" method="POST">
   <!-- Search Bar -->
-  <div class="row">
-      <div class="input-field col s12" >
-	   <i class="material-icons prefix">search</i>
-	   <input type="text" data-ng-model="searchOrder" data-ng-keyup="change()" 
-	   id="promotion" placeholder="Search for #PromotionID, or #PromotionTitle" name="username"
-	   >
-	   </div>
-       <div>
-	   <button><a href="CreatePromotion.php">add</a></button> 
-	   </div>
+	
+  <a href="CreatePromotion.php" class="btn btn-primary float-right mb-3">+</a>
+
+
+<div class="input-group mb-3">
+  <input type="text" class="form-control" name="valueToSearch" placeholder="Search for #PromotionID, or #PromotionTitle" aria-describedby="basic-addon2">
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="submit" name="search" value="Search">Search</button>
   </div>
+</div>
   
   <!-- Table -->
   <div class="table-responsive">
@@ -82,9 +119,17 @@
 				   <th>Last Claimed</th>
 			   </tr>
 		   </thead>
+	        <?php while($row = mysqli_fetch_array($search_result)):?>
+
+		      <tr class="table-row" data-href="">
+			     <td><a href="promoterview.php"><?php echo $row['promo_id'];?></a></td>
+				 <td><a href="promoterview.php"><?php echo $row['promo_title'];?></a></td>
+				 <td>To be added</td>
+			  </tr>
+		   <?php endwhile; ?>
 	   </table>
   </div>
- 
+  </form>
 </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -118,4 +163,4 @@
       })
     </script>
 </body>
-</html>\
+</html>

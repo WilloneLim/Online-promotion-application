21b80bee2ad95a3ca360019fe90b850cd12700c0
WilloneLim <4320719@students.swinburne.edu.my>
2019-05-17 19:55:17 +0800

    Updated SQL DELETED STATISTICS

--- PromoSite/sql/promoalert.sql
+++ PromoSite/sql/promoalert.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2019 at 06:36 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: May 17, 2019 at 01:53 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -64,7 +66,8 @@ CREATE TABLE `application` (
 
 INSERT INTO `application` (`app_id`, `app_email`, `app_username`, `app_password`, `app_desp`, `app_profile`, `app_cover`, `app_key`, `app_date`) VALUES
 (1, 'we@admin.com', 'Wilson and co', 'nothinghere', 'desption', 'some.png', 'cover.png', 'key.png', '2019/09/10'),
-(3, 'ewfwef@gmail.com', 'Rinchinfs', 'rerere', 'Desppint', 'kokok.png', 'cove.png', 'keykay', '2018/07/12');
+(3, 'ewfwef@gmail.com', 'Rinchinfs', 'rerere', 'Desppint', 'kokok.png', 'cove.png', 'keykay', '2018/07/12'),
+(4, 'jojojo@gmail.com', 'Square', '123', 'Cool stuff', '5.jpg', 'explore.jpg', ' drink clothes', '2019/05/16');
 
 -- --------------------------------------------------------
 
@@ -121,7 +124,7 @@ INSERT INTO `promoter` (`promoter_id`, `promoter_username`, `promoter_email`, `p
 (10, 'Dominos', 'domi@email.com', 'lovepizza', 'pizza ', 'food', 'profiledomi.png', 'coverdomi.png'),
 (11, 'Hush Puppies', 'hush@email.com', 'lovepuppies', 'clothes and stuff', 'clothes', 'profilehush.png', 'coverhush.png'),
 (12, 'Sephora', 'sep@email.com', 'lovemakeup', 'desp', 'clothes', 'something.png', 'somethingalso.png'),
-(13, 'Tealive', 'tea@teamail.com', 'lovetea', 'desction', 'drinks', 'profile.png', 'promo1.png'),
+(13, 'Tealive', 'tea@teamail.com', 'lovetea', 'desction', 'drinks', 'profiletea.png', 'promo1.png'),
 (14, 'McDonalds', 'mcd@email.com', 'happymeal', 'this is a desc for mcd', 'food', 'profilemcd.png', 'covermcd.png'),
 (15, 'Loki, God of Mischief', 'loki@hotmail.com', 'loki', 'Save him save ma boi', ' food drink clothes shoe bag other', 'Loki.png', 'Mark1.jpg'),
 (16, 'My cute shop', 'clothes@hotmail.com', 'cute', 'We sell clothes', ' clothes', '4.jpg', 'best.jpg');
@@ -159,27 +162,34 @@ INSERT INTO `promotion` (`promo_id`, `promo_img`, `promo_title`, `promo_desc`, `
 -- --------------------------------------------------------
 
 --
--- Table structure for table `statistics`
+-- Table structure for table `transaction`
 --
 
-CREATE TABLE `statistics` (
-  `id` int(11) NOT NULL,
-  `year` int(11) NOT NULL,
-  `purchase` int(11) NOT NULL,
+CREATE TABLE `transaction` (
+  `trans_id` int(11) NOT NULL,
+  `date` text NOT NULL,
   `sale` int(11) NOT NULL,
-  `profit` int(11) NOT NULL
+  `promo_id` int(11) NOT NULL,
+  `cust_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `statistics`
+-- Dumping data for table `transaction`
 --
 
-INSERT INTO `statistics` (`id`, `year`, `purchase`, `sale`, `profit`) VALUES
-(1, 2014, 2444, 3521, 3555),
-(2, 2015, 2111, 2345, 2545),
-(3, 2016, 2552, 1233, 757),
-(4, 2017, 6767, 5656, 7777),
-(5, 2018, 4324, 556, 442);
+INSERT INTO `transaction` (`trans_id`, `date`, `sale`, `promo_id`, `cust_id`) VALUES
+(1, 'Jan', 1233, 2, 4),
+(2, 'Feb', 3456, 344, 445),
+(3, 'Mar', 3156, 14, 425),
+(4, 'Apr', 6261, 42, 66),
+(5, 'May', 5261, 12, 46),
+(6, 'Jun', 2221, 78, 555),
+(7, 'Jul', 7261, 21, 76),
+(8, 'Aug', 7831, 92, 446),
+(9, 'Sep', 6261, 42, 66),
+(10, 'Oct', 5268, 112, 123),
+(11, 'Nov', 5261, 12, 46),
+(51, 'Dec', 6261, 72, 34);
 
 --
 -- Indexes for dumped tables
@@ -216,12 +226,6 @@ ALTER TABLE `promotion`
   ADD PRIMARY KEY (`promo_id`);
 
 --
--- Indexes for table `statistics`
---
-ALTER TABLE `statistics`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -230,26 +234,32 @@ ALTER TABLE `statistics`
 --
 ALTER TABLE `admin`
   MODIFY `admin_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `application`
 --
 ALTER TABLE `application`
-  MODIFY `app_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `app_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
   MODIFY `cust_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `promoter`
 --
 ALTER TABLE `promoter`
   MODIFY `promoter_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
 --
 -- AUTO_INCREMENT for table `promotion`
 --
 ALTER TABLE `promotion`
   MODIFY `promo_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

791b32b99873d24afd01b7522e69b9473e6dc354
Stella97 <31676244+Stella97@users.noreply.github.com>
2019-05-25 16:28:32 +0800

    delete for promoter account

--- PromoSite/delete.php
+++ PromoSite/delete.php
@@ -1,6 +1,7 @@
 <?php
 include 'connect.php'; 
 
+//deleting the application promoter account
 if(isset($_GET['del_id'])){
     $sql_query="DELETE FROM application WHERE app_id=".$_GET['del_id'];
     
@@ -13,6 +14,17 @@ if(isset($_GET['del_id'])){
 header('Location: applicantview.php');
 $conn->close();
 
-
+//deleting the promoter account
+if(isset($_GET['del_id'])){
+	$sql = "DELETE FROM promoter WHERE promoter_id=".$_GET['del_id'];
+	
+	if(mysqli_query($conn, $sql)){
+		echo "<script>alert 'Record deleted successfully';</script>";
+	}else{
+		echo "Error deleting record: ".mysqli_error($conn);
+	}
+}
+header('Location: adminindex.php');
+$conn->close();
 
 ?>
